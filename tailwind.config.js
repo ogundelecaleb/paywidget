9e643e016def60f0dd618d7c28eb381c201d3533
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-05-31 13:46:48 +0100

    dd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
+ 
   plugins: [],
 };
